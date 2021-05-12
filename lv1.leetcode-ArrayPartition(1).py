d151b0cd58c5087c6bfecad0612e2d0b04846dd2
park-jaehun <pjhun588@gmail.com>
2021-05-12 23:08:05 +0900

    n개의 페어를 이용한 min(a,b)의 합으로 만들 수 있는 가장 큰 수 출력

---
+++ lv1.leetcode-ArrayPartition(1).py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        nums = sorted(nums)
+        result = []
+        for i in range(0, len(nums), 2):
+            result.append(min(nums[i], nums[i + 1]))
+        return (sum(result))
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
