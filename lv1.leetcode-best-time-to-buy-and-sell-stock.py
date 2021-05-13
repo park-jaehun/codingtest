258778d00fa866d9ca9a75f3f14e607abc021a5e
park-jaehun <pjhun588@gmail.com>
2021-05-14 00:53:35 +0900

    주식을 사고 팔기 가장 좋은 시점

---
+++ lv1.leetcode-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        min_price = sys.maxsize
+        # 최대값 초기화, 최소값 초기화  = -sys.maxsize, sys_maxsize
+        profit = 0
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = max(profit, price - min_price)
+        return profit
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
