df51641c5ee62da6088ad65ee47f27c9ed86d310
park-jaehun <pjhun588@gmail.com>
2021-05-31 15:51:45 +0900

    그리드

---
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/lv1.숫자 카드 게임.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+min_result = []
+for i in range(n):
+    # 해당  row의 개수만큼 리스트가 형성됨
+    ls = list(map(int, input().split()))
+    # row가 생길 때마다 min을 리스트로 저장
+    min_result.append(min(ls))
+# 리스트의 최소값으 출력
+print(max(min_result))
+
+
+
+
+
