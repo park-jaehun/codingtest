fe40b1c544dd57531b944a9c16d4509bf4e9349f
park-jaehun <pjhun588@gmail.com>
2021-07-09 08:17:45 +0900

    구현 - 2

---
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/06.시각.py
@@ -0,0 +1,13 @@
+# 자세한 문제 풀이 .....
+# -*- coding: utf-8 -*-
+h = int(input())
+
+count = 0
+for i in range(h + 1):
+    for j in range(60):
+        for k in range(60):
+            # 매 시각 '3이 포함되어 있다면 카운트 증가
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
